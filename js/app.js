ae29a26318716d7e09cebac079cc9a7738ef7837
Raulgaray26 <raulgaray@uees.edu.ec>
2026-09-08 19:37:50 -0500

    feat: agregar formulario para agregar pedidos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,50 @@
 // Controlador: usa el Modelo y actualiza la Vista
-const contenedor = document.querySelector("#menu");
+const contenedor = document.querySelector("#menu-items");
 
 menu.forEach(plato => {
     contenedor.innerHTML += `<div class="menu-item">${plato.nombre}
     - $${plato.precio}</div>`;
 });
+
+// Elementos del formulario
+const inputNombre = document.getElementById("nombre");
+const selectPlato = document.getElementById("plato");
+for (let i=0; i < menu.length; i++){
+    const opcion = document.createElement("option");
+    opcion.value = menu[i].nombre;
+    opcion.textContent = `${menu[i].nombre} - $${menu[i].precio}`;
+    selectPlato.appendChild(opcion);
+}
+const inputCantidad = document.getElementById("cantidad");
+
+// Botón y Tabla
+const btnAgregar = document.getElementById("btn-agregar");
+const tablaPedidos = document.getElementById("tabla-pedidos");
+
+function agregarPedido() {
+    // 1. Leer los valores actuales de los campos
+    const nombre = inputNombre.value;
+    const plato = selectPlato.value;
+    const cantidad = inputCantidad.value;
+
+    // 2. Validar que el nombre no esté vacío
+    if (nombre === "") {
+        alert("Ingresa el nombre del cliente");
+        return;
+    }
+
+    // 3. Insertar la fila y limpiar el formulario
+    const fila = document.createElement("tr");
+    fila.innerHTML =
+    `<td>${nombre}</td>
+    <td>${plato}</td>
+    <td>${cantidad}</td>`;
+    tablaPedidos.appendChild(fila);
+
+    // Limpiar los campos
+    inputNombre.value = "";
+    inputCantidad.value = "1";
+    inputNombre.focus();
+}
+
+btnAgregar.addEventListener("click", agregarPedido);
--- js/data.js
+++ js/data.js
@@ -1,5 +1,8 @@
 // Modelo: datos del menú
 const menu = [
+    { nombre: "Chaulafan de Pollo", precio: 5.99 },
     { nombre: "Chaulafán Mixto", precio: 7.99},
-    { nombre: "Tallarín Saltado Vegetariano", precio: 5.99 }
+    { nombre: "Tallarín Saltado Vegetariano", precio: 5.99 },
+    { nombre: "Tallarín Saltado Mixto", precio: 7.99 },
+    { nombre: "Porción de Wantanes", precio: 2.99 }
 ];
